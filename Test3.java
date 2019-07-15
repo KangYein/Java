deba6fd7c40ac85dd5df324372a2179b91d4ca2c
KangYein <yeinkang04@gmail.com>
2019-07-15 17:05:38 +0900

    7월 15일

---
+++ Test3.java
@@ -0,0 +1,15 @@
+public class Test3 {
+
+	public static void main(String[] args) {
+		double value;	double pi =3.14;
+		value =10+8; System.out.println(value);
+		value =10*pi; System.out.println(value);
+		value =314/pi; System.out.println(value);
+		value =pi-3; System.out.println(value);
+		value =(double)10/3; System.out.println(value);
+		value =10%3; System.out.println(value);
+		value =123%3; System.out.println(value);
+		
+	}
+
+}
