b3dc7a46302b57b334fbc7afbe4b16b58dc14828
KangYein <yeinkang04@gmail.com>
2019-07-15 17:06:10 +0900

    7월 15일

---
+++ Test5.java
@@ -0,0 +1,16 @@
+public class Test5 {
+
+	public static void main(String[] args) {
+		int x=2;
+		int y=2;
+		System.out.println(x==y);
+		System.out.println(x<=y);
+		System.out.println(x>y);
+		System.out.println((x==2)&&(y==2));
+		System.out.println((x==2)&&(y!=2));
+		System.out.println((x==2)||(y!=2));
+		System.out.println((x==5)||(y!=2));
+
+	}
+
+}
