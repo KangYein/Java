30be54e6ed0ee9fcd54f94035359e8b1047bc38e
KangYein <yeinkang04@gmail.com>
2019-07-15 17:05:19 +0900

    7월 15일

---
+++ Test2.java
@@ -0,0 +1,14 @@
+public class Test2 {
+
+	public static void main(String[] args) {
+		byte num1=9;
+		short num2=num1;
+		int num3=num2;
+		long num4=num3;
+		float num5=num4;
+		double num6=num5;
+		System.out.println(num6);
+
+	}
+
+}
