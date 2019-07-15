6253353d8cab22a475420fc19b3883598457dbe0
KangYein <yeinkang04@gmail.com>
2019-07-15 17:05:05 +0900

    7월 15일

---
+++ Test1.java
@@ -0,0 +1,16 @@
+public class Test1 {
+
+	public static void main(String[] args) {
+		int a =10;
+		double b;
+		b=a;	System.out.println(b);
+		//a=b;	System.out.println(a);
+		a=(int)b;	System.out.println(a);
+		b=10/3;		System.out.println(b);
+		b=(double)10/3;	System.out.println(b);
+		b=(int)10.2+(int)3.3;	System.out.println(b);
+		b=(int)10.2+3.3;	System.out.println(b);
+
+	}
+
+}
