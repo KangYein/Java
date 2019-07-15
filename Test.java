41b56f824345f71fc15609e240a5d53096f752a4
KangYein <yeinkang04@gmail.com>
2019-07-15 17:04:50 +0900

    7월 15일

---
+++ Test.java
@@ -0,0 +1,16 @@
+public class Test {
+
+	public static void main(String[] args) {
+		int x=2;
+		int y=2;
+		int value;
+		value =++x*5; System.out.println(value);
+		value =y++*5; System.out.println(value);
+		value =++y; System.out.println(value);
+		value =x++; System.out.println(value);
+		value =y--; System.out.println(value);
+		value =--x; System.out.println(value);
+		
+	}
+
+}
