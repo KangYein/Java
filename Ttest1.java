23d780591ad348f8b9ec82c0fb1234029a3893b0
KangYein <yeinkang04@gmail.com>
2019-07-16 16:19:56 +0900

    7월 16일

---
+++ Ttest1.java
@@ -0,0 +1,13 @@
+public class Ttest1 {
+
+	public static void main(String[] args) {
+		int x=4;
+		//Integer a=4;
+		
+		System.out.println(x);
+		System.out.println(Integer.toBinaryString(x));//이진수로 출력
+		System.out.println(Integer.toBinaryString(-5));
+		//a. 클래스로 만들어짐
+	}
+
+}
