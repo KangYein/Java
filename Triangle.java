333911b1b4eecf02ce906fb0ed246a6b7d4bbc0e
KangYein <yeinkang04@gmail.com>
2019-07-16 16:47:26 +0900

    7월 16일

---
+++ Triangle.java
@@ -0,0 +1,17 @@
+public class Triangle {
+
+	public static void main(String[] args) {
+		int line=4;
+		for(int i=0;i<line;i++) {
+			for(int j=line-i;j>0;j--) {
+				System.out.print(" ");
+			}
+			for(int k=0;k<i*2+1;k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
