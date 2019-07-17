d4e27cd6de5820e2e5bcc354dd121d18b42d0a7a
KangYein <yeinkang04@gmail.com>
2019-07-17 17:00:28 +0900

    7월 17일

---
+++ ArrayTest.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class ArrayTest {
+	final static int STUDENT =5;
+	public static void main(String[] args) {
+		int[] scores=new int[STUDENT];
+		getValue(scores);
+		getAverage(scores);
+	}
+	private static void getValue(int[] array) {
+		Scanner sc =new Scanner(System.in);
+		for(int i=0;i<array.length;i++) {
+			System.out.println("성적을 입력하시오.:");
+			array[i]=sc.nextInt();
+		}
+		sc.close();
+	}
+	private static void getAverage(int[] array) {
+		int total=0;
+		for(int i=0;i<array.length;i++) {
+			total+=array[i];
+		}
+		System.out.println("평균:"+total/array.length);
+	}
+}
