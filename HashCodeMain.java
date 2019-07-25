351ba5d3fb2afcd6e7af4d3cc2eecef9558deb74
KangYein <yeinkang04@gmail.com>
2019-07-26 01:03:49 +0900

    7월25일

---
+++ HashCodeMain.java
@@ -0,0 +1,12 @@
+class HashMan{
+	
+}
+public class HashCodeMain {
+	public static void main(String[] args) {
+		HashMan h1 = new HashMan();
+		HashMan h2 = new HashMan();
+		System.out.println(h1+":"+h1.hashCode());
+		System.out.println(h2+":"+h2.hashCode());
+	}
+
+}
