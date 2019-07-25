da7f855e5012b2fd9ac29a284e1060d076fce824
KangYein <yeinkang04@gmail.com>
2019-07-26 01:04:34 +0900

    7월25일

---
+++ StringEqualsMain.java
@@ -0,0 +1,12 @@
+public class StringEqualsMain {
+	public static void main(String[] args) {
+		String s1 =new String("Hello World");
+		String s2 =new String("Hello World");
+		//결과는 false가 된다.
+		System.out.println("s1 == s2:"+(s1==s2));
+		//결과는 true가 된다.
+		System.out.println("s1.equals(s2):"+s1.equals(s2));
+		System.out.println(s1.hashCode());
+		System.out.println(s2.hashCode());
+	}
+}
