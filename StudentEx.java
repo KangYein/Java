c1c44127df2e921fa8a156ade9a75fe38693ba53
KangYein <yeinkang04@gmail.com>
2019-07-26 01:04:59 +0900

    7월25일

---
+++ StudentEx.java
@@ -0,0 +1,24 @@
+class People{
+	public String name,ssn;
+	public People(String name, String ssn) {
+		this.name=name;
+		this.ssn=ssn;
+	}
+}
+class Student extends People{
+	public int studentNo;
+	public Student(String name, String ssn,int studentNo) {
+		super(name,ssn);
+		this.studentNo =studentNo;
+	}
+}
+public class StudentEx {
+	public static void main(String[] args) {
+		Student student =new Student("ȫ�浿","123456-1234567",1);
+		System.out.println("name :"+student.name);
+		System.out.println("ssn :"+student.ssn);
+		System.out.println("studentNo :"+student.studentNo);
+
+	}
+
+}
